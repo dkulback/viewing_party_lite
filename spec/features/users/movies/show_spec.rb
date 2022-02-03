13883c03258d7a69a923b9c5cca25c23381927b1
Darren Kulback <dkulback@gmail.com>
2022-02-02 22:39:15 -0700

    add vcr to tests

--- spec/features/users/movies/show_spec.rb
+++ spec/features/users/movies/show_spec.rb
@@ -4,25 +4,26 @@ RSpec.describe 'Users movie show page' do
   it 'has buttons to create viewing party and to return to discovery page' do
     user = User.create!(name: 'user', email: 'email')
 
-    visit "/users/#{user.id}/movies/730154"
-    within '.buttons' do
-      expect(page).to have_button("Create Viewing Party")
-      expect(page).to have_button("Discover Page")
-      click_button("Discover Page")
-      expect(current_path).to eq("/users/#{user.id}/discover")
+    VCR.use_cassette('your_eyes_tell') do
+      visit "/users/#{user.id}/movies/730154"
+      within '.buttons' do
+        expect(page).to have_button('Create Viewing Party')
+        expect(page).to have_button('Discover Page')
+        click_button('Discover Page')
+        expect(current_path).to eq("/users/#{user.id}/discover")
+      end
     end
   end
-
   it 'has movie title, average, runtime, genre(s), summary' do
     user = User.create!(name: 'user', email: 'email')
 
     VCR.use_cassette('your_eyes_tell') do
       visit "/users/#{user.id}/movies/730154"
       within '.movie-info' do
-        expect(page).to have_content("Title: Your Eyes Tell")
-        expect(page).to have_content("Runtime: ")
-        expect(page).to have_content("Genre(s): ")
-        expect(page).to have_content("Summary: ")
+        expect(page).to have_content('Title: Your Eyes Tell')
+        expect(page).to have_content('Runtime: ')
+        expect(page).to have_content('Genre(s): ')
+        expect(page).to have_content('Summary: ')
       end
     end
   end
@@ -32,8 +33,8 @@ RSpec.describe 'Users movie show page' do
     VCR.use_cassette('your_eyes_tell_reviews') do
       visit "/users/#{user.id}/movies/730154"
       within '.movie-info' do
-        expect(page).to have_content("Total Reviews: 25")
-        expect(page).to have_content("Author: Fuckface")
+        expect(page).to have_content('Total Reviews: 25')
+        expect(page).to have_content('Author: Fuckface')
       end
     end
   end
