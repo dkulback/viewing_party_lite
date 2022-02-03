67682cc2926da1f97fc383cacb5d9571414c2bc4
Darren Kulback <dkulback@gmail.com>
2022-02-02 21:27:29 -0700

    set_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show discover]
   def new
     @user = User.new
   end
@@ -14,16 +15,20 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @user
   end
 
   def discover
-    @user = User.find(params[:id])
+    @user
   end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :email)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
