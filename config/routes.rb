8f0d87850b92ee50467a288384cd9d7438165b28
Darren Kulback <dkulback@gmail.com>
2022-03-17 17:43:10 -0600

    Adjust bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
 
   resources :users, only: %i[show create]
   get '/dashboard', to: 'users#show'
