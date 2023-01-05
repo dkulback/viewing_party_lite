042d0301b8f0d1393f7ce6f3f89c1e15240cc98c
Darren Kulback <dkulback@gmail.com>
2023-01-04 17:48:45 -0700

    Add set friend id before action

--- app/controllers/user_friends_controller.rb
+++ app/controllers/user_friends_controller.rb
@@ -1,6 +1,13 @@
 class UserFriendsController < ApplicationController
+  before_action :set_friend_id, only: [:create]
   def create
-    Friend.create(user_id: params[:user_id], friend_id: params[:format])
+    Friend.create(user_id: current_user.id, friend_id: set_friend_id.id)
     redirect_to users_path, notice: 'Friend Request Sent!'
+  end
+
+  private
+
+  def set_friend_id
+    @friend_id = User.friendly.find(params[:format])
   end
 end
