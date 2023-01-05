7c2c49fc46de7468bec9b654d9b4b5d4bb8dcf74
Darren Kulback <dkulback@gmail.com>
2023-01-04 17:43:02 -0700

    Add rescue when user doesnt exist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
 
+  def not_found
+    raise ActionController::RoutingError, 'Not Found'
+  rescue StandardError
+    render_404
+  end
+
+  def render_404
+    render file: "#{Rails.root}/public/404", status: :not_found
+  end
+
   def current_user
     @user ||= User.find(session[:user_id]) if session[:user_id]
   end
